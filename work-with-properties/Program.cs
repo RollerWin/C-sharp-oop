14069fd9606e6de89f4597d8e9f7de61d55f86b4
RollerWin <rinatyych@mail.ru>
2024-02-09 03:08:33 +0300

    fixed more problems

--- work-with-properties/Program.cs
+++ work-with-properties/Program.cs
@@ -2,18 +2,50 @@
 {
     static void Main(string[] args)
     {
-        Player player = new Player();
-        Field field = new Field();
+        int positionX = ReadPositiveNumber();
+        int positionY = ReadPositiveNumber();
+
+        Player player = new Player(positionX, positionY);
         Renderer renderer = new Renderer();
 
-        while(true)
+        renderer.DrawPlayer(player.X, player.Y, player.Character);
+    }
+
+    static int ReadPositiveNumber()
+    {
+        string userInput;
+        bool isCorrect = false;
+        int correctNumber = 0;
+
+        while(isCorrect != true)
         {
-            field.DrawField();
-            renderer.UpdateScreen(player.X, player.Y, player.Symbol);
-            ConsoleKeyInfo userInput = Console.ReadKey(true);
-            player.HandleInput(userInput);
-            Console.Clear();
+            userInput = Console.ReadLine();
+
+            if(int.TryParse(userInput, out correctNumber) && correctNumber > 0)
+            {
+                isCorrect = true;
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод! Попробуйте ещё раз");
+                Console.ReadKey();
+                Console.Clear();
+            }
         }
+
+        return correctNumber;
+    }
+}
+
+class Renderer
+{
+    public void DrawPlayer(int x, int y, char playerSymbol)
+    {
+        Console.Clear();
+        Console.CursorVisible = false;
+        Console.SetCursorPosition(x, y);
+        Console.Write(playerSymbol);
+        Console.ReadKey(true);
     }
 }
 
@@ -23,12 +55,6 @@ class Player
     int _positionY;
     char _playerSymbol = '@';
 
-    public Player()
-    {
-        _positionX = Field.MinPositionX + 1;
-        _positionY = Field.MinPositionY + 1;
-    }
-
     public int X
     {
         get
@@ -37,10 +63,7 @@ class Player
         }
         private set
         {
-            if(_positionX + value > Field.MinPositionX && _positionX + value < Field.MaxPositionX - 1)
-            {
-                _positionX += value;
-            }
+            _positionX = value;
         }
     }
 
@@ -52,89 +75,21 @@ class Player
         }
         private set
         {
-            if(_positionY + value > Field.MinPositionY && _positionY + value < Field.MaxPositionY - 1)
-            {
-                _positionY += value;
-            }
+            _positionY = value;
         }
     }
 
-    public char Symbol 
+    public char Character
     {
-        get 
+        get
         {
             return _playerSymbol;
         }
     }
 
-    public void HandleInput(ConsoleKeyInfo input)
+    public Player(int x, int y)
     {
-        const ConsoleKey СommandShiftLeftArrowawMap = ConsoleKey.LeftArrow;
-        const ConsoleKey СommandShiftLeftBoard = ConsoleKey.A;
-
-        const ConsoleKey СommandShiftRightArrow = ConsoleKey.RightArrow;
-        const ConsoleKey СommandShiftRightBoard = ConsoleKey.D;
-
-        const ConsoleKey СommandShiftUpArrow = ConsoleKey.UpArrow;
-        const ConsoleKey СommandShiftUpBoard = ConsoleKey.W;
-
-        const ConsoleKey СommandShiftDownArrow = ConsoleKey.DownArrow;
-        const ConsoleKey СommandShiftDownBoard = ConsoleKey.S;
-
-        switch(input.Key)
-        {
-            case СommandShiftLeftArrowawMap:
-            case СommandShiftLeftBoard:
-                X = -1;
-            break;
-
-            case СommandShiftRightArrow:
-            case СommandShiftRightBoard:
-                X = 1;
-            break;
-
-            case СommandShiftUpArrow:
-            case СommandShiftUpBoard:
-                Y = -1;
-            break;
-
-            case СommandShiftDownArrow:
-            case СommandShiftDownBoard:
-                Y = 1;
-            break;
-        }
-    }
-}
-
-class Field
-{
-    public const int MinPositionX = 0;
-    public const int MinPositionY = 0;
-    public const int MaxPositionX = 10;
-    public const int MaxPositionY = 10;
-    public const char BorderSymbol = '#';
-
-    // private char[] _fieldMap;
-    public void DrawField()
-    {
-        for(int i = MinPositionY; i < MaxPositionY; i++)
-        {
-            for(int j = MinPositionX; j < MaxPositionX; j++)
-            {
-                Console.Write(BorderSymbol);
-            }
-
-            Console.WriteLine();
-        }
-    }
-}
-
-class Renderer
-{
-    public void UpdateScreen(int x, int y, char playerSymbol)
-    {
-        Console.CursorVisible = false;
-        Console.SetCursorPosition(x, y);
-        Console.Write(playerSymbol);  
+        _positionX = x;
+        _positionY = y;
     }
 }

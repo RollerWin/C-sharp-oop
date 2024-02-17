ed403e8e4501272af372713f170de78fdb98bf9c
RollerWin <rinatyych@mail.ru>
2024-02-17 15:58:47 +0300

    progress in work

--- deck-of-cards/Program.cs
+++ deck-of-cards/Program.cs
@@ -8,17 +8,57 @@
 
 class Player
 {
+    public Player() => playerDeck = new List<Card>();
 
+    public List<Card> playerDeck {get; private set;}
+
+    public void AddCardToDeck(Card newCard) => playerDeck.Add(newCard);
 }
 
 class Deck
 {
+    private List<string> _suits = new List<string>{"Червы", "Бубны", "Трефы", "Пики"};
+
+    const int MinCardValue = 1;
+    const int MaxCardValue = 10;
+
     public Deck()
     {
         DeckOfCards = new List<Card>();
+
+        for(int i = MinCardValue; i <= MaxCardValue; i++)
+        {
+            foreach(var suit in _suits)
+            {
+                Card card = new Card(suit, i);
+                DeckOfCards.Add(card);
+            }
+        }
     }
 
-    public List<Card> DeckOfCards {get; private set;} 
+    public List<Card> DeckOfCards {get; private set;}
+
+    public void ShuffleDeck()
+    {
+        Random random = new Random();
+
+        for (int i = 0; i < DeckOfCards.Count - 1; i++)
+        {
+            int j = random.Next(i + 1, DeckOfCards.Count);
+
+            Card temp = DeckOfCards[i];
+            DeckOfCards[i] = DeckOfCards[j];
+            DeckOfCards[j] = temp;
+        }
+    }
+
+    public Card ExtractCard()
+    {
+        Card extractractedCard = DeckOfCards[0];
+        DeckOfCards.RemoveAt(0);
+        
+        return extractractedCard;
+    }
 }
 
 class Card

96852dbb13ce8822bb496d163dad8753e493f621
RollerWin <rinatyych@mail.ru>
2024-02-22 21:48:24 +0300

    fixed solution

--- supermarket/Program.cs
+++ supermarket/Program.cs
@@ -29,9 +29,18 @@ class Client
         return amountOfProducts;
     }
 
-    public int GetNumberOfProducts() => _products.Count;
+    public int GetNumberOfProducts => _products.Count;
 
-    public void RemoveProduct(int removedIndex) => _products.RemoveAt(removedIndex);
+    public void RemoveRandomProduct()
+    {
+        Random random = new Random();
+        int minProductIndex = 0;
+        int maxProductIndex = _products.Count;
+            
+        _products.RemoveAt(random.Next(minProductIndex, maxProductIndex));
+    }
+
+    public bool CanPay() => Money >= GetAmountOfProducts();
 
     public void PayProduct(int amountOfProducts) => Money -= amountOfProducts;
 }
@@ -51,38 +60,70 @@ class Product
 class Supermarket
 {
     private Queue<Client> _clients;
+    private List<Product> _shelvesWithProducts;
 
     public Supermarket()
     {
         _clients  = new Queue<Client>();
+        _shelvesWithProducts = new List<Product>();
+        
+        FillShelves();
+        AddClients();
+    }
 
-        _clients.Enqueue
-        (
-            new Client
-            (
-                500, 
-                new List<Product>
-                {
-                    new Product("a", 50),
-                    new Product("b", 100),
-                    new Product("c", 40)
-                }
-            )
-        );
+    private void FillShelves()
+    {
+        _shelvesWithProducts.Add(new Product("фарш", 100));
+        _shelvesWithProducts.Add(new Product("молоко", 50));
+        _shelvesWithProducts.Add(new Product("кола", 40));
+        _shelvesWithProducts.Add(new Product("сыр", 150));
+        _shelvesWithProducts.Add(new Product("корм для собак", 60));
+        _shelvesWithProducts.Add(new Product("сырок", 30));
+        _shelvesWithProducts.Add(new Product("чипсы", 65));
+        _shelvesWithProducts.Add(new Product("пиво", 20));
+        _shelvesWithProducts.Add(new Product("квас", 25));
+    }
 
-        _clients.Enqueue
-        (
-            new Client
-            (
-                100,
-                new List<Product>
-                {
-                    new Product("a", 20),
-                    new Product("b", 100),
-                    new Product("c", 40)
-                }
-            )
-        );
+    private void AddClients()
+    {
+        int minNumberOfClients = 1;
+        int maxNumberOfClients = 10;
+
+        int minClientMoney = 0;
+        int maxClientMoney = 500;
+
+        Random random = new Random();
+        int numberOfClients = random.Next(minNumberOfClients, maxNumberOfClients + 1);
+        int clientMoney;
+        
+        for(int i = 0; i < numberOfClients; i++)
+        {
+            clientMoney = random.Next(minClientMoney, maxClientMoney + 1);
+            
+            _clients.Enqueue(new Client(clientMoney, FormClientCart(random)));
+        }
+    }
+
+    private List<Product> FormClientCart(Random random)
+    {
+        List<Product> clientCart = new List<Product>();
+
+        int minRandomProductIndex = 0;
+        int maxRandomProductIndex = _shelvesWithProducts.Count;
+        
+        int minNumberOfProducts = 1;
+        int maxNumberOfProducts = 5;
+        
+        int numberOfProducts = random.Next(minNumberOfProducts, maxNumberOfProducts + 1);
+
+        for(int j = 0; j < numberOfProducts; j++)
+        {
+            int randomProductIndex = random.Next(minRandomProductIndex, maxRandomProductIndex);
+            Product randomProduct = _shelvesWithProducts[randomProductIndex];
+            clientCart.Add(new Product(randomProduct.Name, randomProduct.Price));
+        }
+
+        return clientCart;
     }
 
     public void WorkSupermarket()
@@ -91,32 +132,26 @@ class Supermarket
         {
             Client client = _clients.Dequeue();
             int amountOfProducts = client.GetAmountOfProducts();
+            Console.WriteLine($"Клиент пришёл на кассу\nОбщая сумма его товаров: {amountOfProducts}\tДенег у клиента: {client.Money}");
 
-            Console.WriteLine($"Клиент пришёл на кассу\nОбщая сумма его товаров: {amountOfProducts}");
-
-            if(client.Money >= amountOfProducts)
+            if(client.CanPay())
             {
                 client.PayProduct(amountOfProducts);
             }
             else
             {
-                while(client.Money <= amountOfProducts)
+                while(client.CanPay() == false)
                 {
-                    Random random = new Random();
+                    client.RemoveRandomProduct();
 
-                    int minProductIndex = 0;
-                    int maxProductIndex = client.GetNumberOfProducts();
-                    
-                    client.RemoveProduct(random.Next(minProductIndex, maxProductIndex));
                     amountOfProducts = client.GetAmountOfProducts();
-
                     Console.WriteLine($"Теперь товаров на сумму {amountOfProducts}");
                 }
 
                 client.PayProduct(amountOfProducts);
             }
 
-            Console.WriteLine("Все товары оплачены!");
+            Console.WriteLine("Все товары оплачены!\n");
         }
     }
 }

7be180b25b3f93da793063fc79310f48a625a4a9
RollerWin <rinatyych@mail.ru>
2024-03-10 02:32:01 +0300

    added solution

--- garage/Program.cs
+++ garage/Program.cs
@@ -2,47 +2,223 @@
 {
     static void Main(string[] args)
     {
+        CarService carService = new CarService();
+        Queue<Client> clients = new Queue<Client>();
 
+        Console.Write("Введите количество клиентов: ");
+        int userInput = ReadCorrectValue();
+        
+        for(int i = 0; i < userInput; i++)
+            clients.Enqueue(new Client());
+
+        while(clients.Count > 0 && carService.Money >= 0)
+            carService.ServeClient(clients.Dequeue());
+
+        if(clients.Count == 0)
+            Console.WriteLine("Клиенты закончились! Ура!");
+        else if(carService.Money < 0)
+            Console.WriteLine("Денег нет! Игра окончена :(");
+    }
+
+    static int ReadCorrectValue()
+    {
+        int correctValue = 0;
+        bool isCorrect = false;
+
+        while (isCorrect == false)
+        {
+            string userInput = Console.ReadLine();
+
+            if (int.TryParse(userInput, out correctValue) && correctValue > 0)
+            {
+                isCorrect = true;
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод! Попробуйте ещё раз");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+
+        return correctValue;
     }
 }
 
 class CarService
 {
-    public double Money {get; private set;}
+    private const int PenaltyFee = 100;
+
+    private StoreHouse _storeHouse;
+
+    public CarService()
+    {
+        int minServiceMoney = 20;
+        int maxServiceMoney = 400;
+        Money = UserUtils.GenerateRandomValue(minServiceMoney, maxServiceMoney);
+        _storeHouse = new StoreHouse();
+    }
+
+    public int Money {get; private set;}
+
+    public void ServeClient(Client client)
+    {
+        Console.WriteLine($"У клиента поломка {client.BrokenDetail.Name}");
+        Console.WriteLine("Мы имеем детали:");
+        _storeHouse.ShowDetails();
+
+        if(_storeHouse.IsDetailExist(client.BrokenDetail.Name))
+        {
+            int userInput = ReadCorrectIndex(_storeHouse.GetNumberOfDetails());
+            Detail usedDetail = _storeHouse.GetDetail(userInput - 1);
+
+            if(usedDetail.Name == client.BrokenDetail.Name)
+            {
+                Console.WriteLine($"Клиент доволен! Оплата: {usedDetail.Price}");
+                Money += usedDetail.Price;
+            }
+            else
+            {
+                Console.WriteLine($"Деталь не та! Штраф: {PenaltyFee}");
+                Money -= PenaltyFee;
+            }
+        }
+        else
+        {
+            Console.WriteLine($"Деталь отсутствует! Штраф: {PenaltyFee}");
+            Money -= PenaltyFee;
+        }
+
+        Console.WriteLine($"Итоговая сумма: {Money}");
+    }
+
+    private int ReadCorrectIndex(int limit)
+    {
+        int correctValue = 0;
+        bool isCorrect = false;
+
+        while (isCorrect == false)
+        {
+            Console.WriteLine("Выберите номер детали:");
+            string userInput = Console.ReadLine();
+
+            if (int.TryParse(userInput, out correctValue) && correctValue > 0 && correctValue <= limit)
+            {
+                isCorrect = true;
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод! Попробуйте ещё раз");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+
+        return correctValue;
+    }
+}
+
+class Client
+{
+    public Client()
+    {
+        int minClientMoney = 20;
+        int maxClientMoney = 400;
+        Money = UserUtils.GenerateRandomValue(minClientMoney, maxClientMoney);
+        BrokenDetail = DetailsTemplate.GetRandomDetail();
+    }
+
+    public int Money {get; private set;}
+    public Detail BrokenDetail {get; private set;}
 }
 
 class Detail
 {
-    public Detail(string name, double price)
+    public Detail(string name, int price)
     {
         Name = name;
         Price = price;
     }
 
     public string Name {get; private set;}
-    public double Price {get; private set;}
+    public int Price {get; private set;}
 }
 
-class DetailsTemplate
+class StoreHouse
 {
     private List<Detail> _details;
 
-    public DetailsTemplate()
+    public StoreHouse()
     {
         _details = new List<Detail>();
         AddDetails();
     }
 
-    public Detail GetRandomDetail() => _details[UserUtils.GenerateRandomValue(0, _details.Count - 1)];
+    public void ShowDetails()
+    {
+        for(int i = 0; i < _details.Count; i++)
+            Console.WriteLine($"{i + 1}. {_details[i].Name}, Цена: {_details[i].Price}");
+    }
+
+    public Detail GetDetail(int index)
+    {
+        Detail tempDetail = _details[index];
+        _details.RemoveAt(index);
+
+        return new Detail(tempDetail.Name, tempDetail.Price);
+    }
+
+    public bool IsDetailExist(string detailName)
+    {
+        bool isFound = false;
+
+        foreach(var detail in _details)
+            if(detailName == detail.Name)
+                isFound = true;
+
+        return isFound;
+    }
+
+    public int GetNumberOfDetails() => _details.Count;
 
     private void AddDetails()
     {
-        _details.Add(new Detail("Мотор", 200));
-        _details.Add(new Detail("Тормоза", 100));
-        _details.Add(new Detail("Фара", 25));
-        _details.Add(new Detail("Колесо", 50));
-        _details.Add(new Detail("Коробка передач", 75));
-        _details.Add(new Detail("Бампер", 30));
+        int minNumberOfDetails = 5;
+        int maxNumberOfDetails = 15;
+        int numbersOfDetails = UserUtils.GenerateRandomValue(minNumberOfDetails, maxNumberOfDetails + 1);
+
+        for(int i = 0; i < numbersOfDetails; i++)
+            _details.Add(DetailsTemplate.GetRandomDetail());
+    }
+}
+
+static class DetailsTemplate
+{
+    private static List<Detail> s_details;
+
+    static DetailsTemplate()
+    {
+        s_details = new List<Detail>();
+        AddDetails();
+    }
+
+    static public Detail GetRandomDetail()
+    {
+        Detail tempDetail = s_details[UserUtils.GenerateRandomValue(0, s_details.Count - 1)];
+
+        return new Detail(tempDetail.Name, tempDetail.Price);
+    }
+
+    static public int GetNumberOfDetails() => s_details.Count;
+
+    static private void AddDetails()
+    {
+        s_details.Add(new Detail("Мотор", 200));
+        s_details.Add(new Detail("Тормоза", 100));
+        s_details.Add(new Detail("Фара", 25));
+        s_details.Add(new Detail("Колесо", 50));
+        s_details.Add(new Detail("Коробка передач", 75));
+        s_details.Add(new Detail("Бампер", 30));
     }
 }
 
--- garage/obj/Debug/net8.0/garage.AssemblyInfo.cs
+++ garage/obj/Debug/net8.0/garage.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("garage")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+459e6e98fb73cb714bf4f80c9b1c89f3299f56f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+121a885d1bfb7f7033177bd27f0f3950ce826413")]
 [assembly: System.Reflection.AssemblyProductAttribute("garage")]
 [assembly: System.Reflection.AssemblyTitleAttribute("garage")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
